494e68258dd65f9b7e858d8dce8b91e39d6def15
Elwin <Elwin@Win7-PC1.mshome.net>
2018-01-09 23:01:05 +0100

    initial version

---
+++ example1/src/main/java/properties/examples/example1/App.java
@@ -0,0 +1,40 @@
+package properties.examples.example1;
+
+import java.io.File;
+import java.util.List;
+
+import org.apache.commons.configuration2.Configuration;
+import org.apache.commons.configuration2.builder.fluent.Configurations;
+import org.apache.commons.configuration2.ex.ConfigurationException;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) throws ConfigurationException
+    {
+    	Configurations configs = new Configurations();
+    //	try
+   // 	{
+    		System.out.println( "Hello World!" );
+    	    Configuration config = configs.properties(new File("resources\\test1.properties"));
+    	    // access configuration properties
+    	    System.out.println(config.getProperty("prop1.example").toString());
+    	    System.out.println(config.getProperty("testfile2.property2"));
+    	    System.out.println(config.getString("testfile2.property2"));
+    	    List<String> choiceLists = config.getList(String.class, "choicelists");
+    	    
+    	    for (String choiceList : choiceLists) {
+    	    	System.out.println(choiceList.toString());       	    
+    	    }
+    	    
+    	
+    //  	}
+    //	catch (ConfigurationException cex)
+   // 	{
+    	    // Something went wrong
+   // 	}
+    }
+}
